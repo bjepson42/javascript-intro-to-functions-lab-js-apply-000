c3acd59b9465c25a21bc8c4e7af5fa270f4e7062
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-08-28 18:52:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
+<<<<<<< HEAD
   if (string.toUpperCase() === string) {
     string = "YES INDEED!"
   } else if (string.toLowerCase() === string) { 
@@ -22,5 +23,10 @@ function sayHiToGrandma(string){
   } else { 
     string = 'I love you, too.'
   }
+=======
+  if (uppercase.toUpperCase() === string) { string = "YES INDEED!" }
+  if (lowercase.toLowerCase() === string) { string = 'I can\'t hear you!' }
+  if (mixedCase.toLowerCase() === string && mixedCase.toUpperCase === string) { string = 'I love you, too.' }
+>>>>>>> cd4dca081937622563a3c280f2b3dbd5c5aec9f7
   return string
 }
